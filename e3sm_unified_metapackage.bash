033ba81258a635da82eac853c15f4277f9487e45
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-04-13 13:56:39 +0200

    Update versions of many packages for 1.1.4

--- e3sm_unified_metapackage.bash
+++ e3sm_unified_metapackage.bash
@@ -1,35 +1,38 @@
-export UVCDAT_VERSION=2.12
+#!/bin/bash
 export BUILD=0
-export VERSION=1.1.3
+export VERSION=1.1.4
 
 
-conda metapackage -c conda-forge -c e3sm -c acme -c uvcdat \
+conda metapackage -c conda-forge -c e3sm -c cdat \
     e3sm-unified ${VERSION} --build-number ${BUILD}  \
     --dependencies \
-    "cdat_info ==${UVCDAT_VERSION}" \
-    "distarray ==${UVCDAT_VERSION}" \
-    "cdms2 ==${UVCDAT_VERSION}" \
-    "cdtime ==${UVCDAT_VERSION}" \
-    "cdutil ==${UVCDAT_VERSION}" \
-    "genutil ==${UVCDAT_VERSION}" \
-    "vtk-cdat ==7.1.0.${UVCDAT_VERSION}" \
-    "dv3d ==${UVCDAT_VERSION}" \
-    "vcs ==${UVCDAT_VERSION}" \
-    "vcsaddons ==${UVCDAT_VERSION}" \
-    "acme_diags ==1.2.1" \
+    "cdat ==8.0" \
+    "cdat_info ==8.0" \
+    "distarray ==2.12.2" \
+    "cdms2 ==3.0" \
+    "cdtime ==3.0" \
+    "cdutil ==8.0" \
+    "genutil ==8.0" \
+    "vtk-cdat ==8.0.1.8.0" \
+    "dv3d ==8.0" \
+    "vcs ==8.0" \
+    "vcsaddons ==8.0" \
+    "output_viewer ==1.2.3" \
+    "wk ==8.0" \
+    "thermo ==8.0" \
+    "cdp ==1.2.3" \
     "cibots ==0.2" \
-    "output_viewer ==1.2.2" \
-    "xarray ==0.10.0" \
-    "dask ==0.16.0" \
-    "nco ==4.7.3" \
-    "lxml ==4.1.1" \
+    "xarray ==0.10.2" \
+    "dask ==0.17.2" \
+    "nco ==4.7.4" \
+    "lxml ==4.2.1" \
     "sympy ==1.1.1" \
     "pyproj ==1.9.5.1" \
-    "pytest ==3.2.2" \
-    "shapely  ==1.6.2" \
-    "cartopy  ==0.15.1" \
+    "pytest ==3.5.0" \
+    "shapely  ==1.6.4" \
+    "cartopy  ==0.16.0" \
     "progressbar ==2.3" \
-    "pillow ==4.3.0" \
+    "pillow ==5.0.0" \
     "numpy >1.13" \
     "scipy <1.0.0" \
     "matplotlib" \
@@ -40,11 +43,10 @@ conda metapackage -c conda-forge -c e3sm -c acme -c uvcdat \
     "ipython" \
     "plotly" \
     "bottleneck ==1.2.1" \
-    "hdf5 ==1.8.18" \
+    "hdf5 ==1.10.1" \
     "netcdf4 ==1.3.1" \
     "pyevtk ==1.0.1" \
     "f90nml" \
     "globus-cli" \
     "globus-sdk" \
-    "mpas_analysis ==0.7.0" \
-    "processflow ==1.0.1"
+    "mpas_analysis ==0.7.0"
