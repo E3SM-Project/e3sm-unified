722b94fe6a49982e4eb42cf7b5b600ca722fcb7b
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-11-20 12:57:27 -0700

    Update version for 1.2.4

--- e3sm_unified_metapackage.bash
+++ e3sm_unified_metapackage.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-BUILD=1
-VERSION=1.2.3
+BUILD=0
+VERSION=1.2.4
 
 
 conda metapackage -c conda-forge -c e3sm -c cdat \
@@ -21,11 +21,11 @@ conda metapackage -c conda-forge -c e3sm -c cdat \
     "wk ==8.0" \
     "thermo ==8.0" \
     "e3sm_nex ==0.0.2" \
-    "e3sm_diags ==1.4.0" \
+    "e3sm_diags ==1.5.0" \
     "cibots ==0.2" \
     "xarray ==0.10.8" \
     "dask ==0.18.2" \
-    "nco ==4.7.7" \
+    "nco ==4.7.8" \
     "lxml" \
     "sympy" \
     "pyproj" \
@@ -44,18 +44,19 @@ conda metapackage -c conda-forge -c e3sm -c cdat \
     "ipython" \
     "plotly" \
     "bottleneck" \
-    "hdf5 ==1.10.2" \
-    "netcdf4 ==1.4.1=py27ha292673_200" \
+    "hdf5 ==1.10.3" \
+    "netcdf4 ==1.4.2" \
     "evtk ==1.1.1" \
     "f90nml" \
     "globus-cli" \
     "globus-sdk" \
-    "mpas_analysis ==1.0" \
-    "processflow ==2.1.0" \
+    "mpas_analysis ==1.1" \
+    "processflow ==2.1.1" \
     "tabulate" \
     "cmocean" \
     "gsw" \
     "libnetcdf ==4.6.1" \
     "livvkit ==2.1.6" \
     "pyflann" \
-    "scikit-image"
+    "scikit-image" \
+    "shapely"
