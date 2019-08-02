781d68305da74da075bf35ccb8ba6904f25aa571
Joseph H Kennedy <me@jhkennedy.org>
2019-08-02 18:25:53 -0400

    Add first stab at a e3sm simple environment

--- build.bash
+++ build.bash
@@ -3,7 +3,9 @@
 #conda build -c conda-forge -c defaults -c e3sm -c cdat/label/v81 meta.yaml
 
 upload=True
-version=1.2.6
+unified_version=1.2.6
+simple_version=1.0.0
+names=("unified" "simple")
 pythons=(27 36 37)
 build=0
 
@@ -11,6 +13,15 @@ if [ $upload == "True" ]
 then
    for python in "${pythons[@]}"
    do
-      anaconda upload -u e3sm /home/xylar/miniconda3/conda-bld/linux-64/e3sm-unified-${version}-py${python}_${build}.tar.bz2
+      for name in names
+      do
+        if [ $name == "unified" ]
+        then
+          version=$unified_version
+        else
+          version=$simple_version
+        fi
+        anaconda upload -u e3sm /home/xylar/miniconda3/conda-bld/linux-64/e3sm-${name}-${version}-py${python}_${build}.tar.bz2
+      done
    done
 fi
