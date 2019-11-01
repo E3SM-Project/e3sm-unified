be5fc94d83a78f1da71b8f060803050b00e35067
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-11-01 11:24:34 +0100

    Update container definitions and associate CI

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
 export PATH="${GOPATH}/bin:${PATH}"
 
 mkdir -p "${GOPATH}/src/github.com/sylabs"
 cd "${GOPATH}/src/github.com/sylabs"
 
-git clone -b release-3.4 https://github.com/sylabs/singularity
+git clone https://github.com/sylabs/singularity
 cd singularity
+git checkout tags/v3.4.2
 ./mconfig -v -p /usr/local
 make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
 sudo make -C ./builddir install
