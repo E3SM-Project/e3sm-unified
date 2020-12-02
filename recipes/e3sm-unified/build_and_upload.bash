26c17b3a528ac23049e1d0715e36f0d93f551528
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-12-02 13:05:46 +0100

    Add build pins

--- recipes/e3sm-unified/build_and_upload.bash
+++ recipes/e3sm-unified/build_and_upload.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 rm -rf ~/miniconda3/conda-bld
-
-conda build --override-channels -c conda-forge -c defaults -c e3sm .
-
 upload=False
+
+for file in configs/mpi_mpich_python3.7.yaml configs/mpi_nompi_python3.7.yaml
+do
+  conda build -m $file --override-channels -c conda-forge -c defaults -c e3sm .
+done
 
 if [ $upload == "True" ]
 then
