a2e0ac94895181091c67b76c083096a24ec81189
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-01-01 10:32:32 +0100

    Build all version and change build string

--- recipes/e3sm-unified/build_and_upload.bash
+++ recipes/e3sm-unified/build_and_upload.bash
@@ -3,7 +3,7 @@
 rm -rf ~/miniconda3/conda-bld
 upload=False
 
-for file in configs/mpi_mpich_python3.7.yaml configs/mpi_nompi_python3.7.yaml
+for file in configs/mpi_*_python*.yaml
 do
   conda build -m $file --override-channels -c conda-forge -c defaults -c e3sm .
 done
