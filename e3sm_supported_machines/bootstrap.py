a22a8015772754cd700f22b51b31c817a63c6a7a
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-01-24 20:56:25 -0700

    Only use nco_dev label for nco alpha/beta

--- e3sm_supported_machines/bootstrap.py
+++ e3sm_supported_machines/bootstrap.py
@@ -86,7 +86,7 @@ def get_env_setup(args, config, machine):
 
 def build_env(is_test, recreate, compiler, mpi, conda_mpi, version,
               python, conda_base, activ_suffix, env_suffix, activate_base,
-              local_conda_build):
+              local_conda_build, nco_dev):
 
     if compiler is not None:
         build_dir = f'build{activ_suffix}'
@@ -120,9 +120,12 @@ def build_env(is_test, recreate, compiler, mpi, conda_mpi, version,
         channels = '--override-channels'
         if local_conda_build is not None:
             channels = f'{channels} -c {local_conda_build}'
+
+        if nco_dev:
+            channels = f'{channels} -c conda-forge/label/nco_dev'
         channels = f'{channels} -c conda-forge/label/e3sm_dev ' \
-                   f'-c conda-forge/label/nco_dev -c conda-forge ' \
-                   f'-c defaults -c e3sm/label/e3sm_dev -c e3sm'
+                   f'-c conda-forge -c defaults -c e3sm/label/e3sm_dev ' \
+                   f'-c e3sm'
     else:
         channels = '--override-channels -c conda-forge -c defaults -c e3sm'
 
@@ -469,13 +472,16 @@ def main():
     if machine is None:
         compiler = None
 
+    nco_spec = config.get('spack_specs', 'nco')
+    nco_dev = ('alpha' in nco_spec or 'beta' in nco_spec)
+
     nompi_compiler = None
     nompi_suffix = '_nompi'
     # first, make nompi environment
     env_path, env_nompi, _, _, _ = build_env(
         is_test, recreate, nompi_compiler, mpi, 'nompi', version,
         python, conda_base, nompi_suffix, nompi_suffix, activate_base,
-        args.local_conda_build)
+        args.local_conda_build, nco_dev)
 
     if not is_test:
         # make a symlink to the environment
@@ -485,7 +491,7 @@ def main():
     env_path, env_name, activate_env, channels, spack_env = build_env(
         is_test, recreate, compiler, mpi, conda_mpi, version,
         python, conda_base, activ_suffix, env_suffix, activate_base,
-        args.local_conda_build)
+        args.local_conda_build, nco_dev)
 
     sys_info = dict(modules=[],
                     env_vars=['export HDF5_USE_FILE_LOCKING=FALSE'])
