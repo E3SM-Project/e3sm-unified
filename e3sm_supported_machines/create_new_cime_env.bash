9073ce87616c11c8c222bd98e5db31ed9a78739c
Michael Kelleher <kelleher.mike@gmail.com>
2022-03-07 13:22:27 -0600

    Update version of evv4esm, bump cime-env version number

--- e3sm_supported_machines/create_new_cime_env.bash
+++ e3sm_supported_machines/create_new_cime_env.bash
@@ -21,7 +21,7 @@ check_env () {
 
 # Modify the following to choose which cime-env version(s)
 # the python version(s) are installed
-versions=(1.4.0)
+versions=(1.5.0)
 pythons=(3.9)
 
 default_python=3.9
