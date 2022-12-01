8305798747df4f1e84116ecb9c768d7316519746
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-12-01 13:31:47 +0100

    Update to v1.8.0 release

--- e3sm_supported_machines/shared.py
+++ e3sm_supported_machines/shared.py
@@ -14,7 +14,7 @@ except ImportError:
 def parse_args(bootstrap):
     parser = argparse.ArgumentParser(
         description='Deploy E3SM-Unified')
-    parser.add_argument("--version", dest="version", default="1.8.0rc6",
+    parser.add_argument("--version", dest="version", default="1.8.0",
                         help="The version of E3SM-Unified to deploy")
     parser.add_argument("--conda", dest="conda_base",
                         help="Path for the  conda base")
