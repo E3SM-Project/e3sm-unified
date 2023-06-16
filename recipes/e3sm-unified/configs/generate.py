805dc7d0e10e246450c6158e0cb57951dbd4baed
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-06-16 14:50:36 +0200

    Add nompi configurations back

--- recipes/e3sm-unified/configs/generate.py
+++ recipes/e3sm-unified/configs/generate.py
@@ -8,7 +8,7 @@ with open('template.yaml') as f:
 
 template = Template(tempate_test)
 for python in ['3.8', '3.9', '3.10']:
-    for mpi in ['mpich', 'openmpi', 'hpc']:
+    for mpi in ['nompi', 'mpich', 'openmpi', 'hpc']:
         script = template.render(python=python, mpi=mpi)
         filename = f'mpi_{mpi}_python{python}.yaml'
         with open(filename, 'w') as handle:
